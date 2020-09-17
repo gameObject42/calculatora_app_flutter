b6893a87c50cf8e47705b77469e474e1b774b514
Hiki <48960647+gameObject42@users.noreply.github.com>
2020-09-17 10:54:46 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,27 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: new Container());
   }
 }
