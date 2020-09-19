478ed7f7bd6565ddbab746a873bef6d1c838da90
Hiki <48960647+gameObject42@users.noreply.github.com>
2020-09-19 11:05:15 +0530

    Added functionality to the calculator

--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String output = "0";
+
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonValue) {
+    if (buttonValue == "Clear") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonValue == "+" ||
+        buttonValue == "-" ||
+        buttonValue == "/" ||
+        buttonValue == "x") {
+      num1 = double.parse(output);
+      operand = buttonValue;
+      _output = "0";
+    } else if (buttonValue == ".") {
+      if (_output.contains(".")) {
+        print("Already contains a decimal");
+      } else {
+        _output = _output + buttonValue;
+      }
+    } else if (buttonValue == "=") {
+      num2 = double.parse(output);
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "x") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonValue;
+    }
+    print(_output);
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
   Widget buildButton(String buttontext) {
     return new Expanded(
       child: new OutlineButton(
         padding: new EdgeInsets.all(24.0),
-        child: new Text(buttontext),
-        onPressed: () => {},
+        child: new Text(
+          buttontext,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        onPressed: () => buttonPressed(buttontext),
         color: Colors.grey,
         textColor: Colors.black,
       ),
@@ -51,7 +106,18 @@ class _MyHomePageState extends State<MyHomePage> {
         body: new Container(
             child: new Column(
           children: <Widget>[
-            new Text("OutPut"),
+            new Container(
+              alignment: Alignment.centerRight,
+              padding: new EdgeInsets.symmetric(
+                vertical: 132,
+                horizontal: 12,
+              ),
+              child: new Text(output,
+                  style: new TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
             new Expanded(
               child: new Divider(),
             ),
